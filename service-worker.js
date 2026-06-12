cf6e8efcfd918962614e8371fb6fce42b47ff07a
raviranjan kumar <raviranjanzkrdctr0559@gmail.com>
2026-06-12 16:00:39 +0530

    App website

--- service-worker.js
+++ service-worker.js
@@ -25,3 +25,46 @@ self.addEventListener("fetch", (event) => {
     })
   );
 });
+const CACHE_NAME = "edu-hub-pro-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/game.html",
+  "/notes.html",
+  "/manifest.json",
+  "/icon.png"
+];
+
+// INSTALL
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// ACTIVATE
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      );
+    })
+  );
+});
+
+// FETCH (OFFLINE SUPPORT)
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
