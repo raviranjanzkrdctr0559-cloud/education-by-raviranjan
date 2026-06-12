1043a7f3f73944da49064ab007644a0da3bd4c39
raviranjan kumar <raviranjanzkrdctr0559@gmail.com>
2026-06-12 14:55:44 +0530

    App website

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,24 @@
-self.addEventListener("install", () => {
-  console.log("App installed");
+const CACHE_NAME = "edu-hub-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/manifest.json",
+  "/icon.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
