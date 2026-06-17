3a7d4e9f9f9043ba9af3cf7b6571a5012eb1eced
raviranjan kumar <raviranjanzkrdctr0559@gmail.com>
2026-06-17 17:43:08 +0530

    App website

--- service-worker.js
+++ service-worker.js
@@ -1,30 +1,3 @@
-const CACHE_NAME = "edu-hub-v2";
-
-const urlsToCache = [
-  "/",
-  "/index.html",
-  "/notes.html",
-  "/manifest.json",
-  "/icon.png"
-];
-
-// INSTALL
-self.addEventListener("install", (event) => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      return cache.addAll(urlsToCache);
-    })
-  );
-});
-
-// FETCH (offline support)
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
-});
 const CACHE_NAME = "edu-hub-pro-v1";
 
 const urlsToCache = [
@@ -36,7 +9,6 @@ const urlsToCache = [
   "/icon.png"
 ];
 
-// INSTALL
 self.addEventListener("install", (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
@@ -45,7 +17,6 @@ self.addEventListener("install", (event) => {
   );
 });
 
-// ACTIVATE
 self.addEventListener("activate", (event) => {
   event.waitUntil(
     caches.keys().then((keys) => {
@@ -60,7 +31,6 @@ self.addEventListener("activate", (event) => {
   );
 });
 
-// FETCH (OFFLINE SUPPORT)
 self.addEventListener("fetch", (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
